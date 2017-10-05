7ff2283497f6e7663c367ae36a1c8b3a5e067a91
Felipe Acosta <facosta0787@gmail.com>
2017-10-05 00:59:27 -0400

    [update] finished the game of Ahorcadito

--- ahorcado.py
+++ ahorcado.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import random
+import sys
 
 IMAGE = [
 '''
@@ -68,7 +69,7 @@ IMAGE = [
         ========='''
 ]
 
-WORDS = ['Java','Python','JavaScript','Go','C','Csharp','Cobol','Pascal','Ruby','Scala','VisualBasic','PHP','Perl']
+WORDS = ['java','python','javascript','go','c','csharp','cobol','pascal','ruby','scala','visualbasic','php','perl']
 
 
 def randomWorld():
@@ -88,7 +89,37 @@ def run():
     while True:
         tableBoard(hiddenWord,tries)
         currentLetter = str(input('Ingresa una letra: '))
+        letterIndexes = []
+        for i in range(len(word)):
+            if word[i] == currentLetter:
+                letterIndexes.append(i)
+        if len(letterIndexes) == 0:
+            tries += 1
+            if tries == (len(IMAGE) - 1):
+                tableBoard(hiddenWord,tries)
+                print('')
+                print('¡ Perdiste ! la palabra correcta era {}'.format(word))
+                break # Detiene los loops
+        else:
+            for idx in letterIndexes:
+                hiddenWord[idx] = currentLetter
+        letterIndexes = []
+        try:
+            hiddenWord.index('-')
+        except ValueError:
+            print('')
+            print('Felicidades! Ganaste. La palabra es {}'.format(word))
+            break
+
+    resp = str(input('Deseas volver a jugar? type (yes/no): '))
+    if resp == 'y' or resp == 'yes' or resp == 'Y' or resp == 'Yes':
+        run()
+    else:
+        sys.exit(0)
+
+
+
 
 if __name__ == ('__main__'):
-    print('B I E N V E N I D O S  A  A H O R C A D I T O')
+    print('B I E N V E N I D O S  A L  A H O R C A D I T O')
     run()
